342e2f44c0eb00b4823f6b35160f75845cfee125
ANHTRAN <tran36a@mtholyoke.edu>
2023-11-06 21:29:18 -0500

    finish print bikes and stations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,5 +3,11 @@ class StationsController < ApplicationController
   def index
     @stations = Station.all.order(identifier: :asc)
   end
+
+  def show
+    @station = Station.find(params[:id])
+  rescue ActiveRecord::RecordNotfound
+    redirect_to root_path
+  end
   
 end
