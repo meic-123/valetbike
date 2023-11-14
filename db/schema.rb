4dafa3bbba842153944e2a48abcf5b7cad916f08
Anna <achristen3386@gmail.com>
2023-11-14 16:10:15 -0500

    troubleshooting nomethod error

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_11_12_204351) do
-
   create_table "bikes", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "identifier"
     t.integer "current_station_id"
@@ -76,15 +74,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_204351) do
     t.date "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "membership_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.integer "membership_id"
   end
 
 end
