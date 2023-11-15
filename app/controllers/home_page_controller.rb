4e874178a09f3f81028758a07057979a09134e06
Sami Aiyathurai <53624695+Sami-Aiyathurai@users.noreply.github.com>
2023-11-15 18:51:29 -0500

    Fixed stations printing

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,8 +3,14 @@ class HomePageController < ApplicationController
   end
 
   def map
+    if params[:station].present?
+      @stations = Station.near(params[:station])
+    else
       @stations = Station.all.order(identifier: :asc)
+    end
   end
 
+  def about
+  end
   
 end
