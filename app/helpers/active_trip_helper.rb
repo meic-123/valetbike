62827f1a88443ce08143fe085873aa888d2ec226
Anna <achristen3386@gmail.com>
2023-12-03 14:07:57 -0500

    added customizeable user account and sign up pages with devise

--- app/helpers/active_trip_helper.rb
+++ app/helpers/active_trip_helper.rb
@@ -1,12 +1,2 @@
 module ActiveTripHelper
-    def change_bike_avail
-        @user = current_user
-        @trip = Trip.find_by(user_id: @user.id)
-        @bike = Bike.find(@user.active_bike)
-        #@station = @trip.ending_station_id
-        @bike.avail = true
-        @user.active_bike = nil
-        @user.save
-        root_path
-      end
 end
