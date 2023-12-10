acf97e1d9cb8bd1007d0f10e53157607e6868044
Sami Aiyathurai <53624695+Sami-Aiyathurai@users.noreply.github.com>
2023-12-10 13:40:42 -0500

    Fixed database usage and worked on renting styling

--- app/models/station.rb
+++ app/models/station.rb
@@ -8,4 +8,9 @@ class Station < ApplicationRecord
   has_many :started_trips, class_name: :Trip, foreign_key: :start_station_id
   has_many :ended_trips, class_name: :Trip, foreign_key: :end_station_id
 
+  def bikenum
+    docked_bikes.where(avail: 'true').count
+  end 
+
+
 end
